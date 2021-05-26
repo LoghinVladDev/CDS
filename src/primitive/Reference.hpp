75d8bb11bb3255f9bbf3d789c4df510fdf339421
LoghinVladDev <vlad.loghin00@gmail.com>
2021-05-27 02:54:07 +0300

    Template Partial Spec fix

--- src/primitive/Reference.hpp
+++ src/primitive/Reference.hpp
@@ -29,14 +29,14 @@ private:
 public:
 
     template < class V, class = decltype (
-        dataTypes::refWrapper::hiddenRef<T>(std::declval<V>()),
-        std::enable_if_t < ! std::is_same < Reference,
+    dataTypes::refWrapper::hiddenRef<T>(std::declval<V>()),
+            std::enable_if_t < ! std::is_same < Reference,
 #if __cpp_lib_remove_cvref >= 201711
-            std::remove_cvref_t <V>
+                    std::remove_cvref_t <V>
 #else
-            std::remove_cv_t<std::remove_reference_t<V>>
+                    std::remove_cv_t<std::remove_reference_t<V>>
 #endif
-        > ::value >()
+            > ::value >()
     ) > constexpr Reference ( V && v ) noexcept(noexcept(dataTypes::refWrapper::hiddenRef<T>(std::forward<V>(v)))) : // NOLINT(google-explicit-constructor,bugprone-forwarding-reference-overload)
             p ( std::addressof( dataTypes::refWrapper::hiddenRef<T>(std::forward<V>(v)))) {
 
@@ -76,11 +76,11 @@ public:
         std::stringstream oss;
         oss << "< " << (std::is_const<T>::value ? "const " : "") << "& of 0x" << std::hex
             << reinterpret_cast < typename PointerBase<T>::PointerType > ( p ) << std::dec << " : " <<
-#if defined(CDS_GLM)
-           isVec () ? String(*p) : (*p)
-#else
+            #if defined(CDS_GLM)
+            (isVec () ? String(*p) : (*p))
+            #else
             (*p)
-#endif
+            #endif
             << " >";
         return String(oss.str());
     }
