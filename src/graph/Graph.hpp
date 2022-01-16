df2466baad6ad7daa77f05875a98641ff11a53b6
Loghin Vlad-Andrei <vlad.loghin00@gmail.com>
2022-01-17 00:59:57 +0200

    Comments regarding graph impl

--- src/graph/Graph.hpp
+++ src/graph/Graph.hpp
@@ -33,18 +33,22 @@ namespace cds{
 
         /// Those methods are just a prototype. Further improvements and changes will occur
 
+        /** returning an uniquePointer implies that a new graph is created. Perhaps returning Graph & was intended */
         auto addEdge ( VertexType const & sourceVertex, VertexType const & destVertex )                 noexcept ( false ) -> UniquePointer < Graph >;
 
         auto getEdge ( VertexType const & sourceVertex, VertexType const & destVertex )                 const noexcept ( false ) -> EdgeType;
 
+        /* consider 'contains' as the base prototype, and overloading it with contains ( vertex, vertex ) and contains ( edge ) */
         auto containsEdge ( VertexType const & sourceVertex, VertexType const & destVertex )            const noexcept ( false ) -> bool;
 
+        /** library standard uses 'allEdgesBetween' instead of 'getAllEdgesBetween', like in 'degreeOf'. Also if no other case than ALL Edges exists, consider naming 'edgesBetween' */
         template < EdgeType T = E , EnableIf < T != MULTIGRAPH > = 0 >
         auto getAllEdgesBetween ( VertexType const & sourceVertex, VertexType const & destVertex  )     const noexcept ( false )-> UnorderedSet < EdgeType >;
 
         template< EdgeType T = E, EnableIf < T == MULTIGRAPH > >
         auto getAllEdgesBetween ( VertexType const & sourceVertex, VertexType const & destVertex )      const noexcept ( false ) -> List < EdgeType >;
 
+        /* if 'getEdgeSet' has the same functionality as 'getAllEdgesBetween', consider removing prototype */
         template < EdgeType T = E , EnableIf < T != MULTIGRAPH > = 0 >
         auto getEdgeSet ( VertexType const & sourceVertex, VertexType const & destVertex  )             const noexcept ( false )-> UnorderedSet < EdgeType >;
 
@@ -52,8 +56,11 @@ namespace cds{
         auto getEdgeSet ( VertexType const & sourceVertex, VertexType const & destVertex )              const noexcept ( false ) -> List < EdgeType >;
 
 
+        /** returning an uniquePointer implies that a new graph is created. Perhaps returning Graph & was intended */
         auto addVertex ( VertexType const & vertex )                                                    noexcept ( false ) -> UniquePointer < Graph >;
 
+        /** returning an uniquePointer implies that a new vertex is created. Perhaps returning VertexType & was intended ( library standard ), or maybe ForeignPointer < VertexType > ( implies no new vertex is created ).
+         * Also, if parameter is vertex, why get it in the first place ? */
         auto getVertex ( VertexType const & vertex )                                                    noexcept ( false ) -> UniquePointer< VertexType >;
 
         auto containsVertex ( VertexType const & vertex )                                               const noexcept ( false ) -> bool;
@@ -64,6 +71,7 @@ namespace cds{
 
         auto outDegreeOf (VertexType const & vertex )                                                   const noexcept ( false ) -> int;
 
+        /* if returned value contains all vertices of object, consider UnorderedSet < VertexType > const & */
         auto getVertexSet ( )                                                                           const noexcept ( false ) -> UnorderedSet < VertexType >;
 
     };
