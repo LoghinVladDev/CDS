0cd9ebcd70fe59f142f7f457c96d971cb1bbd155
Loghin Vlad-Andrei <vlad.loghin00@gmail.com>
2022-05-16 21:28:11 +0300

    Errors caused by win filesystem case sensitivity fixed

---
+++ src/experimental/String.hpp
@@ -0,0 +1,21 @@
+//
+// Created by loghin on 5/16/22.
+//
+
+#ifndef __CDS_STRING_HPP__
+#define __CDS_STRING_HPP__
+
+#include <CDS/experimental/Object>
+
+namespace cds {
+    namespace experimental {
+
+        class String : public Object {
+        public:
+            auto retardedPrivateFunction () noexcept -> void;
+        };
+
+    }
+}
+
+#endif // __CDS_STRING_HPP__
--- tests2.cpp
+++ tests2.cpp
@@ -1,21 +1,26 @@
 #include <CDS/Thread>
 #include <CDS/smartPointers/UniquePointer>
+#include <CDS/experimental/String>
 
 using namespace cds;
 
 int main () {
 
-    UniquePointer < Thread > t = new Runnable ( [& t]{
+    cds::experimental::String s;
+    s.retardedPrivateFunction();
 
-        uint32 repeats = 10;
-
-        for ( uint32 i = 0; i < repeats; ++ i ) {
-            (void) t->sleep ( 500 );
-            std :: cout << "Slept for 500ms" << '\n';
-        }
-    });
-
-    t->start();
-    t->join();
+//
+//    UniquePointer < Thread > t = new Runnable ( [& t]{
+//
+//        uint32 repeats = 10;
+//
+//        for ( uint32 i = 0; i < repeats; ++ i ) {
+//            (void) t->sleep ( 500 );
+//            std :: cout << "Slept for 500ms" << '\n';
+//        }
+//    });
+//
+//    t->start();
+//    t->join();
     return 0;
 }
