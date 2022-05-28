b197b4416fe2d6cf573b1698f8339f477e5c8fd3
Loghin Vlad-Andrei <vlad.loghin00@gmail.com>
2022-05-28 23:38:18 +0300

    Comments for meta/base

--- src/experimental/Object.hpp
+++ src/experimental/Object.hpp
@@ -15,31 +15,67 @@ namespace cds {
 
     namespace experimental {
 
+        /**
+         * @class Base class of all CDS objects
+         */
         class Object {
-        public:
-            __CDS_NoDiscard virtual auto toString () const noexcept -> String;
-            __CDS_NoDiscard virtual auto equals (Object const & object) const noexcept -> bool {
-                return this == & object;
-            }
 
-            friend auto operator << (std::ostream &, Object const &) noexcept -> std::ostream &;
+        public:
+            /**
+             * @brief String conversion function, used to obtain String representation of the object
+             * @exceptsafe
+             * @return String = string representation
+             */
+            __CDS_NoDiscard virtual auto toString () const noexcept -> String;
+
+        public:
+            /**
+             * @brief Explicit Comparison Function with generic CDS/Object
+             * @param object : Object cref = Constant Reference to an Object-derived instance
+             * @exceptsafe
+             * @return bool = true if objects are equal, false otherwise
+             */
+            __CDS_NoDiscard __CDS_cpplang_VirtualConstexpr virtual auto equals (
+                    Object const & object
+            ) const noexcept -> bool;
+
+        public:
+            /**
+             * @brief Operator << overload for std :: ostream types, used to easily print String representation of object
+             * @param out : std :: ostream ref = Reference to a std :: ostream type or type-derived object
+             * @param object : Object cref = Constant Reference to an Object-derived instance
+             * @exceptsafe
+             * @return std :: ostream ref = Reference to received 'out' parameter
+             */
+            friend auto operator << (
+                    std :: ostream        & out,
+                    Object          const &
+            ) noexcept -> std::ostream &;
+
+        public:
+            /**
+             * @brief String cast operator, used to obtain String representation of the object
+             * @exceptsafe
+             * @return String = string representation
+             */
             virtual explicit operator String () const noexcept;
 
-            __CDS_NoDiscard virtual auto hash () const noexcept -> Index {
-                return 0;
-            }
+        public:
+            /**
+             * @brief Hash function
+             * @exceptsafe
+             * @return Index = hash code value of the instance
+             */
+            __CDS_NoDiscard __CDS_cpplang_VirtualConstexpr virtual auto hash () const noexcept -> Index;
 
+        public:
+            /**
+             * @brief implicit destructor
+             * @exceptsafe
+             */
             virtual ~Object () noexcept = default;
         };
 
-    }
-
-}
-
-namespace cds {
-
-    template <> auto hash < experimental :: Object > ( experimental :: Object const & object ) noexcept -> Index {
-        return object.hash();
     }
 
 }
