99472829cff401980a536c3409b007fe2f1013fb
StefanPaulet <tudor.stefan.paulet@gmail.com>
2022-09-19 14:45:47 +0300

    Final commit after all tests for TreeSet work with valgrind

--- tests2.cpp
+++ tests2.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 
 #include <CDS/experimental/TreeSet>
+#include <CDS/String>
 
 int main () {
 
-    cds :: experimental :: TreeSet < int > t { 1, 2, 3, 4, 5};
-    cds :: experimental :: TreeSet < int > x ( t );
-    t.clear();
+    cds :: experimental :: TreeSet < cds :: String > underTest = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    std :: cout << ( cds :: String ( "2" ) > cds :: String ( "5" ) ) << '\n';
+    cds :: experimental :: TreeSet < cds :: String > ceva = { 91245, 9, 2, 5532, 5, 1, 4, 647, 1324, 3, 7, 45, 234, 2365, 2436, 56, 8, 6 };
+    std :: cout << ceva << '\n';
     return 0;
 }
