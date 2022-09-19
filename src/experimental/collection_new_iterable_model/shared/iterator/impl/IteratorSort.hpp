e97c5b2c517c6b21ff6360eac0da02cdb1a2634c
Loghin Vlad-Andrei <vlad.loghin00@gmail.com>
2022-09-19 23:25:10 +0300

    intro-sort implementation

--- src/experimental/collection_new_iterable_model/shared/iterator/impl/IteratorSort.hpp
+++ src/experimental/collection_new_iterable_model/shared/iterator/impl/IteratorSort.hpp
@@ -12,13 +12,99 @@ namespace cds {                 /* NOLINT(modernize-concat-nested-namespaces) */
 
                 namespace __helpers { /* NOLINT(bugprone-reserved-identifier) */
 
+#if defined (__CDS_compiler_gcc) || defined (__CDS_compiler_clang)
+
+                    constexpr auto __lg ( sint32 n ) noexcept -> uint8 {    /* NOLINT(bugprone-reserved-identifier) */
+                        return (uint8) sizeof ( sint32 ) * 8U - 1U - __builtin_clz(n);
+                    }
+
+                    constexpr auto __lg ( uint32 n ) noexcept -> uint8 {    /* NOLINT(bugprone-reserved-identifier) */
+                        return (uint8) sizeof ( uint32 ) * 8U - 1U - __builtin_clz(n);
+                    }
+
+                    constexpr auto __lg ( sint64 n ) noexcept -> uint8 {    /* NOLINT(bugprone-reserved-identifier) */
+                        return (uint8) sizeof ( sint64 ) * 8U - 1U - __builtin_clzll(n);
+                    }
+
+                    constexpr auto __lg ( uint64 n ) noexcept -> uint8 {    /* NOLINT(bugprone-reserved-identifier) */
+                        return (uint8) sizeof ( uint64 ) * 8U - 1U - __builtin_clzll(n);
+                    }
+
+#else
+
+                    inline auto __lg ( sint32 n ) noexcept -> uint8 {   /* NOLINT(bugprone-reserved-identifier) */
+
+                        constexpr static uint32 const bases[]   = { 0x0U, 0x00000002U, 0x0000000CU, 0x000000F0U, 0x0000FF00U, 0xFFFF0000U };
+                        constexpr static uint8  const counts[]  = { 0x0U, 0x01U, 0x02U, 0x04U, 0x08U, 0x10U, 0x20U };
+
+                        uint8 r = 0U;
+                        for ( uint32 i = 5; i > 0; -- i ) {
+                            if ( n & bases[i] ) {
+                                n >>= counts [i];
+                                r |=  counts [i];
+                            }
+                        }
+
+                        return r;
+                    }
+
+                    inline auto __lg ( uint32 n ) noexcept -> uint8 {   /* NOLINT(bugprone-reserved-identifier) */
+
+                        constexpr static uint32 const bases[]   = { 0x0U, 0x00000002U, 0x0000000CU, 0x000000F0U, 0x0000FF00U, 0xFFFF0000U };
+                        constexpr static uint8  const counts[]  = { 0x0U, 0x01U, 0x02U, 0x04U, 0x08U, 0x10U };
+
+                        uint8 r = 0U;
+                        for ( uint32 i = 5; i > 0; -- i ) {
+                            if ( n & bases[i] ) {
+                                n >>= counts [i];
+                                r |=  counts [i];
+                            }
+                        }
+
+                        return r;
+                    }
+
+                    inline auto __lg ( sint64 n ) noexcept -> uint8 {   /* NOLINT(bugprone-reserved-identifier) */
+
+                        constexpr static uint64 const bases[]   = { 0x0U, 0x0000000000000002U, 0x000000000000000CU, 0x00000000000000F0U, 0x000000000000FF00U, 0x00000000FFFF0000U, 0xFFFFFFFF00000000 };
+                        constexpr static uint8  const counts[]  = { 0x0U, 0x01U, 0x02U, 0x04U, 0x08U, 0x10U, 0x20U };
+
+                        uint8 r = 0x0U;
+                        for ( uint32 i = 6; i > 0; -- i ) {
+                            if ( n & bases[i] ) {
+                                n >>= counts [i];
+                                r |=  counts [i];
+                            }
+                        }
+
+                        return r;
+                    }
+
+                    inline auto __lg ( uint64 n ) noexcept -> uint8 {   /* NOLINT(bugprone-reserved-identifier) */
+
+                        constexpr static uint64 const bases[]   = { 0x0U, 0x0000000000000002U, 0x000000000000000CU, 0x00000000000000F0U, 0x000000000000FF00U, 0x00000000FFFF0000U, 0xFFFFFFFF00000000 };
+                        constexpr static uint8  const counts[]  = { 0x0U, 0x01U, 0x02U, 0x04U, 0x08U, 0x10U, 0x20U };
+
+                        uint8 r = 0x0U;
+                        for ( uint32 i = 6; i > 0; -- i ) {
+                            if ( n & bases[i] ) {
+                                n >>= counts [i];
+                                r |=  counts [i];
+                            }
+                        }
+
+                        return r;
+                    }
+
+#endif
+
                     template <
-                            typename __T,
+                            typename __T,                                   /* NOLINT(bugprone-reserved-identifier) */
                             cds :: meta :: EnableIf <
                                     cds :: meta :: isMoveAssignable < __T > () &&
                                     cds :: meta :: isMoveConstructible < __T > ()
                             > = 0
-                    > __CDS_cpplang_ConstexprNonLiteralReturn auto __swap (
+                    > __CDS_cpplang_ConstexprNonLiteralReturn auto __swap ( /* NOLINT(bugprone-reserved-identifier) */
                             __T & left,
                             __T & right
                     ) noexcept -> void {
@@ -30,12 +116,12 @@ namespace cds {                 /* NOLINT(modernize-concat-nested-namespaces) */
 
 
                     template <
-                            typename __T,
+                            typename __T,                                   /* NOLINT(bugprone-reserved-identifier) */
                             cds :: meta :: EnableIf <
                                     ! cds :: meta :: isMoveAssignable < __T > () ||
                                     ! cds :: meta :: isMoveConstructible < __T > ()
                             > = 0
-                    > __CDS_cpplang_ConstexprNonLiteralReturn auto __swap (
+                    > __CDS_cpplang_ConstexprNonLiteralReturn auto __swap ( /* NOLINT(bugprone-reserved-identifier) */
                             __T & left,
                             __T & right
                     ) noexcept -> void {
@@ -43,6 +129,33 @@ namespace cds {                 /* NOLINT(modernize-concat-nested-namespaces) */
                         __T auxiliary   = left;
                         left            = right;
                         right           = auxiliary;
+                    }
+
+                    template <
+                            typename __T,                                       /* NOLINT(bugprone-reserved-identifier) */
+                            cds :: meta :: EnableIf <
+                                    cds :: meta :: isMoveAssignable < __T > ()
+                            > = 0
+                    > __CDS_cpplang_ConstexprNonLiteralReturn auto __assign (   /* NOLINT(bugprone-reserved-identifier) */
+                            __T & left,
+                            __T & right
+                    ) noexcept -> void {
+
+                        left = std :: move ( right );
+                    }
+
+
+                    template <
+                            typename __T,                                           /* NOLINT(bugprone-reserved-identifier) */
+                            cds :: meta :: EnableIf <
+                                    ! cds :: meta :: isMoveAssignable < __T > ()
+                            > = 0
+                    > __CDS_cpplang_ConstexprNonLiteralReturn auto __assign (       /* NOLINT(bugprone-reserved-identifier) */
+                            __T & left,
+                            __T & right
+                    ) noexcept -> void {
+
+                        left = right;
                     }
 
                 }
@@ -122,6 +235,54 @@ namespace cds {                 /* NOLINT(modernize-concat-nested-namespaces) */
 
                 }
 
+                namespace __introSortHelpers { /* NOLINT(bugprone-reserved-identifier) */
+
+                    template <
+                            typename                                                            __RandomAccessIteratorType, /* NOLINT(bugprone-reserved-identifier) */
+                            typename                                                            __Comparator,               /* NOLINT(bugprone-reserved-identifier) */
+                            Size                                                                __threshold,                /* NOLINT(bugprone-reserved-identifier) */
+                            __SortFunction < __RandomAccessIteratorType, __Comparator >         __atThreshold,              /* NOLINT(bugprone-reserved-identifier) */
+                            __SortFunction < __RandomAccessIteratorType, __Comparator >         __atDepth,                  /* NOLINT(bugprone-reserved-identifier) */
+                            __PartitionFunction < __RandomAccessIteratorType, __Comparator >    __partition                 /* NOLINT(bugprone-reserved-identifier) */
+                    > inline auto __introSort (                                                                             /* NOLINT(bugprone-reserved-identifier) */
+                            __RandomAccessIteratorType  const & begin,
+                            __RandomAccessIteratorType  const & end,
+                            __Comparator                const & comparator,
+                            Size                                depth
+                    ) noexcept -> void {
+
+                        auto const length = end - begin;
+
+                        if ( length < __threshold ) {
+                            __atThreshold ( begin, end, comparator );
+                        } else if ( depth == 0U ) {
+                            __atDepth ( begin, end, comparator );
+                        } else {
+
+                            auto const pivot = __partition ( begin, end, comparator );
+
+                            __introSort <
+                                    __RandomAccessIteratorType,
+                                    __Comparator,
+                                    __threshold,
+                                    __atThreshold,
+                                    __atDepth,
+                                    __partition
+                            > ( begin, pivot, comparator, depth - 1 );
+
+                            __introSort <
+                                    __RandomAccessIteratorType,
+                                    __Comparator,
+                                    __threshold,
+                                    __atThreshold,
+                                    __atDepth,
+                                    __partition
+                            > ( pivot + 1, end, comparator, depth - 1 );
+                        }
+                    }
+
+                }
+
                 template <
                         typename __RandomAccessIteratorType,    /* NOLINT(bugprone-reserved-identifier) */
                         typename __Comparator                   /* NOLINT(bugprone-reserved-identifier) */
@@ -135,6 +296,99 @@ namespace cds {                 /* NOLINT(modernize-concat-nested-namespaces) */
                     __heapSortHelpers :: __deHeapifyInPlace ( begin, end - 1, comparator );
                 }
 
+
+                template <
+                        typename __BidirectionalIteratorType,   /* NOLINT(bugprone-reserved-identifier) */
+                        typename __Comparator                   /* NOLINT(bugprone-reserved-identifier) */
+                > inline auto __insertionSort (                 /* NOLINT(bugprone-reserved-identifier) */
+                        __BidirectionalIteratorType const & begin,
+                        __BidirectionalIteratorType const & end,
+                        __Comparator                const & comparator
+                ) noexcept -> void {
+
+                    auto start = begin;
+                    while ( ++ start != end ) {
+                        auto previous1 = start;
+                        auto previous2 = previous1;
+                        -- previous2;
+
+                        while ( previous1 > begin && comparator ( * previous1, * previous2 ) ) {
+                            __helpers :: __swap ( * previous1, * previous2 );
+                            previous1 = previous2;
+                            -- previous2;
+                        }
+                    }
+                }
+
+
+                template <
+                        typename                                                            __RandomAccessIteratorType, /* NOLINT(bugprone-reserved-identifier) */
+                        typename                                                            __Comparator,               /* NOLINT(bugprone-reserved-identifier) */
+                        Size                                                                __threshold,                /* NOLINT(bugprone-reserved-identifier) */
+                        __SortFunction < __RandomAccessIteratorType, __Comparator >         __atThreshold,              /* NOLINT(bugprone-reserved-identifier) */
+                        __SortFunction < __RandomAccessIteratorType, __Comparator >         __atDepth,                  /* NOLINT(bugprone-reserved-identifier) */
+                        __PartitionFunction < __RandomAccessIteratorType, __Comparator >    __partition                 /* NOLINT(bugprone-reserved-identifier) */
+                > inline auto __introSort (                                                                             /* NOLINT(bugprone-reserved-identifier) */
+                        __RandomAccessIteratorType  const & begin,
+                        __RandomAccessIteratorType  const & end,
+                        __Comparator                const & comparator
+                ) noexcept -> void {
+
+                    __introSortHelpers :: __introSort <
+                            __RandomAccessIteratorType,
+                            __Comparator,
+                            __threshold,
+                            __atThreshold,
+                            __atDepth,
+                            __partition
+                    > (
+                            begin,
+                            end,
+                            comparator,
+                            __helpers :: __lg ( (uint64) ( end - begin ) )
+                    );
+                }
+
+
+                template <
+                        typename                                                            __RandomAccessIteratorType, /* NOLINT(bugprone-reserved-identifier) */
+                        typename                                                            __Comparator                /* NOLINT(bugprone-reserved-identifier) */
+                > inline auto __m3Partition (                                                                           /* NOLINT(bugprone-reserved-identifier) */
+                        __RandomAccessIteratorType  const & begin,
+                        __RandomAccessIteratorType  const & end,
+                        __Comparator                const & comparator
+                ) noexcept -> __RandomAccessIteratorType {
+
+                    auto left   = begin;
+                    auto right  = end - 1;
+                    auto mid    = begin + ( right - begin ) / 2U;
+
+                    if ( comparator ( * mid, * left ) ) {
+                        __helpers :: __swap ( * mid, * left );
+                    }
+
+                    if ( comparator ( * right, * left ) ) {
+                        __helpers :: __swap ( * right, * left );
+                    }
+
+                    if ( comparator ( * right, * mid ) ) {
+                        __helpers :: __swap ( * right, * mid );
+                    }
+
+                    __helpers :: __swap ( * mid, * ( -- right ) );
+                    auto piv = right;
+
+                    for ( ;; ) {
+                        while ( left < right && comparator ( * ( ++ left ), * piv ) );
+                        while ( left < right && comparator ( * piv, * ( -- right ) ) );
+                        if ( left < right ) { __helpers :: __swap ( * left, * right ); }
+                        else { break; }
+                    }
+
+                    __helpers :: __swap ( * piv, * left );
+                    return left;
+                }
+
             }
         }
 
@@ -142,13 +396,54 @@ namespace cds {                 /* NOLINT(modernize-concat-nested-namespaces) */
         template <
                 typename __RandomAccessIteratorType,    /* NOLINT(bugprone-reserved-identifier) */
                 typename __Comparator                   /* NOLINT(bugprone-reserved-identifier) */
-        > inline auto heapSort (                      /* NOLINT(bugprone-reserved-identifier) */
+        > inline auto heapSort (                        /* NOLINT(bugprone-reserved-identifier) */
                 __RandomAccessIteratorType  const & begin,
                 __RandomAccessIteratorType  const & end,
                 __Comparator                const & comparator
         ) noexcept -> void {
 
             return __hidden :: __impl :: __heapSort < __RandomAccessIteratorType, __Comparator > (
+                    begin,
+                    end,
+                    comparator
+            );
+        }
+
+
+        template <
+                typename __BidirectionalIteratorType,   /* NOLINT(bugprone-reserved-identifier) */
+                typename __Comparator                   /* NOLINT(bugprone-reserved-identifier) */
+        > inline auto insertionSort (                   /* NOLINT(bugprone-reserved-identifier) */
+                __BidirectionalIteratorType const & begin,
+                __BidirectionalIteratorType const & end,
+                __Comparator                const & comparator
+        ) noexcept -> void {
+
+            return __hidden :: __impl :: __insertionSort < __BidirectionalIteratorType, __Comparator > (
+                    begin,
+                    end,
+                    comparator
+            );
+        }
+
+
+        template <
+                typename __RandomAccessIteratorType,    /* NOLINT(bugprone-reserved-identifier) */
+                typename __Comparator                   /* NOLINT(bugprone-reserved-identifier) */
+        > inline auto introSort (                       /* NOLINT(bugprone-reserved-identifier) */
+                __RandomAccessIteratorType  const & begin,
+                __RandomAccessIteratorType  const & end,
+                __Comparator                const & comparator
+        ) noexcept -> void {
+
+            return __hidden :: __impl :: __introSort <
+                    __RandomAccessIteratorType,
+                    __Comparator,
+                    16U,
+                    & __hidden :: __impl :: __insertionSort < __RandomAccessIteratorType, __Comparator >,
+                    & __hidden :: __impl :: __heapSort < __RandomAccessIteratorType, __Comparator >,
+                    & __hidden :: __impl :: __m3Partition < __RandomAccessIteratorType, __Comparator >
+            > (
                     begin,
                     end,
                     comparator
--- tests2.cpp
+++ tests2.cpp
@@ -8,14 +8,17 @@
 
 int main () {
 
+    using namespace cds :: experimental;
+    std :: cout << (int) __hidden :: __impl :: __helpers :: __lg(17) << '\n';
+
     cds :: experimental :: TreeSet < cds :: String > underTest = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     std :: cout << ( cds :: String ( "2" ) > cds :: String ( "5" ) ) << '\n';
     cds :: experimental :: TreeSet < cds :: String > ceva = { 91245, 9, 2, 5532, 5, 1, 4, 647, 1324, 3, 7, 45, 234, 2365, 2436, 56, 8, 6 };
     std :: cout << ceva << '\n';
     using namespace cds :: experimental;
 
-    Array < int > a = { 9, 1, 2, 7, 4, 5, 8, 1 ,2 ,8 ,9, 7, 0, 4, 5, 1, 2 };
-    heapSort (
+    Array < int > a = { 9, 1, 2, 7, 4, 5, 8, 1 ,2 ,8 ,9, 7, 0, 4, 5, 1, 2,5, 2, 3, 8,7,7,5,2,5,4,4,8,6,1,1,1,5,1,8,65,2,1,5,8,96,4,2,4,7,8,5,6,4,1,5,8,7 };
+    introSort (
             a.data(),
             a.data() + a.size()
     );
