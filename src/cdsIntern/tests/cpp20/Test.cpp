812e6f3fa18978eec2dc714d7d7d92ef62adb483
Loghin Vlad-Andrei <vlad.loghin00@gmail.com>
2022-10-23 03:45:52 +0300

    CDS-53: Modernize, Update CDS/Threading #107 - Updated testing lib

--- src/cdsIntern/tests/cpp20/Test.cpp
+++ src/cdsIntern/tests/cpp20/Test.cpp
@@ -48,7 +48,7 @@ static Test::TerminalColor bold (Test::TerminalColor::Modifier::ENABLE_BOLD );
 #include <iostream>
 #include <cstdio>
 #include <cstdarg>
-#include <CDS/Mutex>
+#include <CDS/threading/Mutex>
 
 static Mutex printLock;
 
