dcc4dca255387559ae480f0b3c9b5d5331880c55
Loghin Vlad-Andrei <vlad.loghin00@gmail.com>
2022-11-05 16:05:52 +0200

    LazySequence clients

--- src/experimental/cds_69_modernize_sequence/Sequence.hpp
+++ src/experimental/cds_69_modernize_sequence/Sequence.hpp
@@ -18,6 +18,8 @@
 #include "sequence/lazySequence/ConstIterator.hpp"
 #include "sequence/lazySequence/Constructs.hpp"
 
+#include "sequence/LazySequence.hpp"
+
 /* TODO : indices of client */
 /* TODO : Generic Statements */
 /* TODO : Add apply, also, first, one to Generic Statements */
@@ -26,60 +28,6 @@
 /* TODO : Drop Implemented with filter */
 
 namespace cds {
-
-    namespace __hidden {
-        namespace __impl {
-
-            template < typename __ElementType > /* NOLINT(bugprone-reserved-identifier, cert-dcl37-c, cert-dcl51-cpp) */
-            class __LazySequence :              /* NOLINT(bugprone-reserved-identifier, cert-dcl37-c, cert-dcl51-cpp) */
-                    public cds :: Iterable < __ElementType > {
-
-            private:
-                friend class __LazySequenceConstIterator < __ElementType >;
-
-            public:
-                cds :: Array < cds :: functional :: Predicate < __ElementType const & > > _filters;
-
-            private:
-                cds :: Iterable < __ElementType > const & _baseIterable;
-
-            private:
-                using BaseIterable = cds :: Iterable < __ElementType >;
-
-            private:
-                using typename BaseIterable :: __GenericHandler;
-
-            private:
-                using typename BaseIterable :: __GenericConstHandler;
-
-            private:
-                __CDS_NoDiscard __CDS_cpplang_ConstexprOverride auto __iicch_obtainGenericHandler (
-                        __IterableInternalRequestType requestType
-                ) noexcept -> __GenericHandler override;
-
-            private:
-                __CDS_NoDiscard __CDS_cpplang_ConstexprOverride auto __iicch_obtainGenericConstHandler (
-                        __IterableInternalRequestType requestType
-                ) const noexcept -> __GenericConstHandler override;
-
-            public:
-                __LazySequence () noexcept = delete;
-
-            public:
-                __LazySequence (
-                        cds :: Iterable < __ElementType > const & iterable
-                ) noexcept;
-
-            public:
-                ~__LazySequence() noexcept override = default;
-
-            public:
-                template < typename __Predicate >
-                __CDS_NoDiscard auto filter ( __Predicate const & predicate ) noexcept -> __LazySequence &;
-            };
-
-        }
-    }
 
     template < typename __ElementType > /* NOLINT(bugprone-reserved-identifier, cert-dcl37-c, cert-dcl51-cpp) */
     class Sequence : public Object {
--- src/experimental/cds_69_modernize_sequence/sequence/lazySequence/Constructs.hpp
+++ src/experimental/cds_69_modernize_sequence/sequence/lazySequence/Constructs.hpp
@@ -9,7 +9,82 @@ namespace cds {
     namespace __hidden {
         namespace __impl {
 
+            template < typename __ElementType >
+            using __LazySequenceDelegateForwardIterableClient =
+                    __LocalDelegateForwardConstIterablePrimitiveClient <
+                            __LazySequence < __ElementType >,
+                            __LazySequenceConstIterator < __ElementType >
+                    >;
 
+
+            template < typename __ElementType >
+            using __LazySequenceContainsOfIterableClient =
+                    __LocalContainsOfCompositeClient <
+                            __LazySequence < __ElementType >,
+                            __ElementType,
+                            cds :: Iterable < __ElementType >,
+                            & __iterableContains < __ElementType >
+                    >;
+
+
+            template < typename __ElementType >
+            using __LazySequenceContainsOfInitializerListClient =
+                    __LocalContainsOfCompositeClient <
+                            __LazySequence < __ElementType >,
+                            __ElementType,
+                            std :: initializer_list < __ElementType >,
+                            & __initializerListContains <
+                                    __ElementType,
+                                    & cds :: meta :: equals < __ElementType >
+                            >
+                    >;
+
+
+            template < typename __ElementType >
+            using __LazySequenceFindOfIterableClient =
+                    __LocalFindOfImmutableCompositeClient <
+                            __LazySequence < __ElementType >,
+                            __ElementType,
+                            __LazySequenceConstIterator < __ElementType >,
+                            cds :: Iterable < __ElementType >,
+                            & __iterableContains < __ElementType >,
+                            & __iterableNotContains < __ElementType >
+                    >;
+
+
+            template < typename __ElementType >
+            using __LazySequenceFindOfInitializerListClient =
+                    __LocalFindOfImmutableCompositeClient <
+                            __LazySequence < __ElementType >,
+                            __ElementType,
+                            __LazySequenceConstIterator < __ElementType >,
+                            std :: initializer_list < __ElementType >,
+                            & __initializerListContains <
+                                    __ElementType,
+                                    & cds :: meta :: equals < __ElementType >
+                            >,
+                            & __initializerListNotContains <
+                                    __ElementType,
+                                    & cds :: meta :: equals < __ElementType >
+                            >
+                    >;
+
+
+            template < typename __ElementType >
+            using __LazySequenceFindByClient =
+                    __LocalFindByImmutableCompositeClient <
+                            __LazySequence < __ElementType >,
+                            __ElementType,
+                            __LazySequenceConstIterator < __ElementType >
+                    >;
+
+
+            template < typename __ElementType >         /* NOLINT(bugprone-reserved-identifier, cert-dcl37-c, cert-dcl51-cpp) */
+            using __LazySequenceGenericStatementsClient = /* NOLINT(bugprone-reserved-identifier, cert-dcl37-c, cert-dcl51-cpp) */
+                    __LocalGenericImmutableStatementsCompositeClient <
+                            __LazySequence < __ElementType >,
+                            __ElementType
+                    >;
 
         }
     }
