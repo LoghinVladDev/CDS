b2f95cd674427ba8f83418d7bfc5a1862a16a773
Vlad-Andrei Loghin <vlad.loghin00@gmail.com>
2024-12-16 16:53:06 +0200

    DCR working on mac 4

--- test/unit/valid/primitive/string_view/StringView_char8_t_x86.cpp
+++ test/unit/valid/primitive/string_view/StringView_char8_t_x86.cpp
@@ -1,5 +1,5 @@
 // DCR-TEST
-// STEPS: compile(linux:gcc;linux:clang;apple:clang;apple:gcc),run(linux:gcc;linux:clang;apple:clang;apple:gcc)
+// STEPS: compile(linux:gcc;linux:clang),run(linux:gcc;linux:clang)
 // STD: 20+
 // FLAGS[compile(linux:*)]: -m32
 
